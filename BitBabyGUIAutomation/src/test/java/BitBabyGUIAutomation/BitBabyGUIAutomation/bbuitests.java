f8312f1f497b9e975054b1632147156186106373
Ahmed Bilal <ahmed.bilal@wearenova.co.uk>
2023-05-18 18:35:05 +0500

    Main Screen elements sorted/Navigation of main CTAs Done!

--- BitBabyGUIAutomation/src/test/java/BitBabyGUIAutomation/BitBabyGUIAutomation/bbuitests.java
+++ BitBabyGUIAutomation/src/test/java/BitBabyGUIAutomation/BitBabyGUIAutomation/bbuitests.java
@@ -9,6 +9,7 @@ import org.testng.annotations.BeforeTest;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterTest;
 
@@ -16,18 +17,12 @@ import org.testng.annotations.AfterTest;
 
 
 public class bbuitests {
-  
+
 	AppiumDriver driver;
-	
-	@Test
-  public void LoginScreen() {
-		
-		System.out.println("BB App Started!");
-		
-		
-  }
-  @BeforeTest
-  public void beforeTest() throws MalformedURLException {{
+
+
+	@BeforeTest
+	public void beforeTest() throws MalformedURLException {{
 		DesiredCapabilities cap=  new DesiredCapabilities();
 		cap.setCapability("devicename", "A13");
 		cap.setCapability("udid", "R58T80RPT9F");
@@ -36,14 +31,79 @@ public class bbuitests {
 		cap.setCapability("appPackage", "com.nova.bitbaby");
 		cap.setCapability("appActivity", "com.nova.bitbaby.MainActivity");
 		URL url = new URL("http://127.0.0.1:4723/wd/hub"); 
-		
-		driver = new AppiumDriver(url,cap);	
-		
-	}
-  }
 
-  @AfterTest
-  public void afterTest() {
-  }
+		driver = new AppiumDriver(url,cap);	
+
+	}
+	}
+
+
+	@Test
+	public void LoginScreen() throws InterruptedException {
+
+		System.out.println("BB App Started!");
+		Thread.sleep(7000);
+
+		String Signin_label=driver.findElement(By.xpath("//android.widget.TextView[@index='1' and @text='Sign In']")).getText();
+		System.out.println("Sign in label has returned this text->"+Signin_label);
+
+		String forgotpassword_text=driver.findElement(By.xpath("//android.widget.TextView[@index='0' and @text='Forgot password']")).getText();
+		System.out.println("Forgot Password CTA has returned this text->"+forgotpassword_text);
+
+		String Signup_text=driver.findElement(By.xpath("//android.widget.TextView[@index='0' and @text='Sign up']")).getText();
+		System.out.println("Sign up CTA has returned this text->"+Signup_text);
+
+		String Partner_register_text=driver.findElement(By.xpath("//android.widget.TextView[@index='0' and @text='Register ']")).getText();
+		System.out.println("Register as partner CTA has returned this text->"+Partner_register_text);
+
+
+		//clicking forgot password CTA
+		driver.findElement(By.xpath("//android.widget.TextView[@index='0' and @text='Forgot password']")).click();
+		Thread.sleep(3000);
+		System.out.println("Forgot password tapped successfully!");
+
+		String Temporary_password_label=driver.findElement(By.xpath("//android.widget.TextView[@index='1' and @text='Temporary password']")).getText();
+		System.out.println("Temporary password label has returned this text->"+Temporary_password_label);
+
+		Thread.sleep(2000);
+
+		System.out.println("Now going back to login screen again!");
+
+		driver.navigate().back();
+		
+		//tapping on sign up as mother
+		driver.findElement(By.xpath("//android.widget.TextView[@index='0' and @text='Sign up']")).click();
+		Thread.sleep(3000);
+		System.out.println("Sign up tapped successfully!");
+
+		String Signupscreen_label=driver.findElement(By.xpath("//android.widget.TextView[@index='1' and @text='Tell us your email address']")).getText();
+		System.out.println("Signup screen label has returned this text->"+Signupscreen_label);
+		
+		Thread.sleep(2000);
+
+		System.out.println("Now going back to login screen again!");
+
+		driver.navigate().back();
+		
+		//tapping on sign up as partner
+		driver.findElement(By.xpath("//android.widget.TextView[@index='0' and @text='Register ']")).click();
+		Thread.sleep(3000);
+		System.out.println("Register as partner CTA tapped successfully!");
+
+		String Registerscreen_label=driver.findElement(By.xpath("//android.widget.TextView[@index='1' and @text='Tell us your email address']")).getText();
+		System.out.println("Signup screen label has returned this text->"+Registerscreen_label);
+		
+		Thread.sleep(2000);
+
+		System.out.println("Now going back to login screen again!");
+
+		driver.navigate().back();
+	}
+
+	@AfterTest
+	public void afterTest() {
+		
+		driver.quit();
+	}
 
 }
